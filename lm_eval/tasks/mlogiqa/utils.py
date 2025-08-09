e142a9982b6b9de86832af8e402b9d676294013f
María Grandury <mariagrandury@gmail.com>
2025-08-09 21:11:30 +0200

    adapt prompt used to eval mode

--- lm_eval/tasks/mlogiqa/utils.py
+++ lm_eval/tasks/mlogiqa/utils.py
@@ -1,15 +1,18 @@
 import ast
+
 import datasets
+
 
 def doc_to_choice(doc):
     """Parse options string to list of choices."""
     return [f"{chr(ord('A') + i)}. {x}" for i, x in enumerate(doc["options"])]
 
+
 def process_docs(dataset: datasets.Dataset) -> datasets.Dataset:
     return dataset.map(lambda x: {"options": ast.literal_eval(x["options"])})
 
 
-def doc_to_text(doc):
+def doc_to_text_mcq(doc):
     """Parse options and render the text template."""
     # # Parse the options string into a list
     options = doc["options"]
@@ -23,5 +26,25 @@ def doc_to_text(doc):
     text += f"C. {options[2]}\n"
     text += f"D. {options[3]}\n"
     text += "Please choose the most suitable one among A, B, C and D as the answer to this question."
+
+    return text
+
+
+def doc_to_text_gen(doc):
+    """Parse options and render the text template."""
+    # Parse the options string into a list
+    options = ast.literal_eval(doc["options"])
+
+    # Build the text template
+    text = f"Passage: {doc['context'].strip()}\n"
+    text += f"Question: {doc['question'].strip()}\n"
+    text += "Choices:\n"
+    text += f"A. {options[0]}\n"
+    text += f"B. {options[1]}\n"
+    text += f"C. {options[2]}\n"
+    text += f"D. {options[3]}\n"
+    text += "Please choose the most suitable one among A, B, C and D as the answer to this question, and return it in the following JSON format:\n"
+    text += "{'answer': '[choice]'}\n"
+    text += "where [choice] must be one of A, B, C and D."
 
     return text
